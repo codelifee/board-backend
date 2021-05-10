fcde663ef11cf20cd104a0af448d6af57e9db211
LAPTOP-8OAON1FJ\siyun <siyun_work@boomco.org>
2021-05-10 16:29:22 +0900

    rest api 나머지 항목 추가

--- model/board.js
+++ model/board.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const mariaDB = require('../lib/mariadb');
 
-//유저 단일정보
+//게시물 전체가져오기
 const boardall = function(){
     return mariaDB.select().from('si_board');
 }
 
+//게시물 상세가져오기
+const boarddetail = function(id){    
+    return mariaDB.select().from('si_board').where('board_no', id);
+}
+
+//게시물 등록
+const boardinsert = function(data){    
+    return mariaDB('si_board').insert({user_no: 1, board_title: data.title, board_content: data.content});
+}
+
+//게시물 수정
+const boardupdate = function(data){    
+    return mariaDB('si_board').where('board_no', data.board_no).update({board_title: data.title, board_content: data.content});
+}
+
+//게시물 삭제
+const boarddelete = function(data){    
+    return mariaDB('si_board').where('board_no', data.board_no).del();
+}
 
 module.exports = {
-    boardall: boardall
+    boardall: boardall,
+    boarddetail: boarddetail,
+    boardinsert: boardinsert,
+    boardupdate: boardupdate,
+    boarddelete: boarddelete
 }
--- routes/board.js
+++ routes/board.js
@@ -7,5 +7,25 @@ router.get('/all', async function(req, res, next){
     res.json(await knex_query.boardall());
 });
 
+//게시물 상세
+router.get('/:id', async function(req, res, next){    
+    res.json(await knex_query.boarddetail(req.params.id));
+});
+
+//게시물 등록
+router.post('/', async function(req, res, next){    
+    res.json(await knex_query.boardinsert(req.body));
+});
+
+//게시물 수정
+router.put('/:id', async function(req, res, next){
+    res.json(await knex_query.boardupdate(req.body));    
+});
+
+//게시물 삭제
+router.delete('/', async function(req, res, next){    
+    res.json(await knex_query.boarddelete(req.body));
+});
+
 //
 module.exports = router;
